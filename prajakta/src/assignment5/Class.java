3c0615a3c7e3263a62f3fe25200ca58392a3b341
hp <hp@LAPTOP-5DB55JU9>
2022-11-10 13:09:11 +0530

    hey

---
+++ prajakta/src/assignment5/Class.java
@@ -0,0 +1,10 @@
+package assignment5;
+	import java.lang.*;
+	public class Class{  
+	public static void main(String args[]){  
+	String s1="HeLlo";  
+	String s1lower=s1.toLowerCase();  
+	System.out.println(s1lower);  
+	}}  
+
+
--- prajakta/src/prajakta/Praju.java
+++ prajakta/src/prajakta/Praju.java
@@ -1,5 +1,41 @@
+
+
 package prajakta;
 
-public class Praju {
+class Circle extends Praju //Circle is child class 
+{
+	public void getArea() // method
+	{
+		int r=7;
+		double  pi = 3.14;
+		double area =pi*r*r;    // calculate area of circle
+		System.out.println(area);
+		
+	}
+}
+class Triangle extends Praju//Triangle is child class 
+{
+	public void getArea() // method
+	{
+		int b=56;
+		int h=34;
+		double area=(b*h)/2;   // calculate area of triangle
+		System.out.println(area);
+	}
+}
+
+public class Praju  // parent class
+{
+
+	public static void main(String[] args)
+	{
+		Circle c = new Circle();  // create object
+		System.out.println("Area of Circle :");
+		c.getArea(); // method call
+		System.out.println("Area of Triangle");
+		Triangle t = new Triangle();  // create object
+		t.getArea();   // method call
+
+	}
 
 }
